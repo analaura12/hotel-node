ebc94dbbad4cb41376797be6710fea71dd1cb26f
brubandrade <61695979+brubandrade@users.noreply.github.com>
2021-11-22 20:45:41 -0300

    Add files via upload

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -6,37 +6,19 @@ const { Reserve } = require('../models');
 const { Router } = require('express');
 const router = Router();
 
-let quartos = [{
-        number: 266,
-        value: 150.99,
-        number_of_guests: 3,
-        company_name: 'Grand Hyatt São Paulo'
-    },
-    {
-        number: 12,
-        value: 120.00,
-        number_of_guests: 1,
-        company_name: 'The Yellow Capsule'
-    }
-];
-/*
-let reservas = [{
-        id: 1,
-        number: 10,
-        initial_date: '2021-11-27',
-        final_date: '2021-11-30'
-    },
-    {
-        id: 2,
-        number: 50,
-        initial_date: '2021-12-27',
-        final_date: '2021-12-30'
-    }
-];
-*/
 router.get('/', async(req, res) => {
-    const reservas = await Reserve.findAll();
-    res.render('user/home', { quartos, reservas });
+    try {
+        const reserves = await Reserve.findAll();
+        const hotels = await Hotel.findAll();
+        const accommodation = await Accommodation1.findAll();
+        res.render('user/home', { reserves, hotels, accommodation });
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+router.get('/registration', (req, res) => {
+    res.render('user/registration');
 });
 
 router.get('/newReserve', (req, res) => {
@@ -45,38 +27,75 @@ router.get('/newReserve', (req, res) => {
 
 router.get('/:id', async(req, res) => {
     const { id } = req.params;
-    let var_reserve = await Reserve.findByPk(id);
-    res.render('user/reserve', { var_reserve });
+
+    try {
+        let var_reserve = await Reserve.findByPk(id);
+        res.render('user/reserve', { var_reserve });
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 router.get('/:id/editReserve', async(req, res) => {
     const { id } = req.params;
-    let var_reserve = await Reserve.findByPk(id);
-    res.render('user/editReserve', { var_reserve });
+
+    try {
+        let var_reserve = await Reserve.findByPk(id);
+        res.render('user/editReserve', { var_reserve });
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+router.post('/new', async(req, res) => {
+    const { id, number, initial_date, final_date } = req.body;
+
+    try {
+        await Reserve.create({ id, number, initial_date, final_date });
+        res.redirect('/user');
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 router.post('/', async(req, res) => {
-    const { id, number, initial_date, final_date } = req.body;
-    await Reserve.create({ id, number, initial_date, final_date });
-    res.redirect('/user');
+    const { first_name, last_name, cpf, birth_date, cellphone, city, state, password, email } = req.body;
+    var _str = birth_date;
+    _new = _str.split('-').reverse().join('-');
+    console.log(_new);
+
+    try {
+        await User.create({ first_name, last_name, cpf, birth_date, cellphone, city, state, password, email });
+        res.redirect('/user');
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 router.patch('/:id', async(req, res) => {
     const initial_date = req.body.initial_date;
     const final_date = req.body.final_date;
 
-    await Reserve.update({ initial_date, final_date }, {
-        where: { id: req.params.id }
-    });
-
-    res.redirect('/user');
+    try {
+        await Reserve.update({ initial_date, final_date }, {
+            where: { id: req.params.id }
+        });
+        res.redirect('/user');
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 router.delete('/:id', async(req, res) => {
-    await Reserve.destroy({
-        where: { id: req.params.id }
-    });
-    res.redirect('/user');
+
+    try {
+        await Reserve.destroy({
+            where: { id: req.params.id }
+        });
+        res.redirect('/user');
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 module.exports = router;

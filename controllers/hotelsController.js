3730bac08d4c689668a6709eca092f786a480dc8
brubandrade <61695979+brubandrade@users.noreply.github.com>
2021-11-24 20:56:57 -0300

    Add files via upload

--- controllers/hotelsController.js
+++ controllers/hotelsController.js
@@ -6,4 +6,28 @@ router.get('/', (req, res) => {
     res.render('hotel/home');
 });
 
+router.get('/profile/:id', async(req, res) => {
+    const { id } = req.params;
+    try {
+        let var_hotel = await Hotel.findByPk(id);
+        res.render('hotel/profile', { var_hotel });
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+router.patch('/:id', async(req, res) => {
+    const { company_name, cnpj, telephone, email, password, address, number, district, cep, city, state, country, convenience, observation, social_reason } = req.body;
+    console.log("foooi");
+    try {
+        await Hotel.update({ company_name, cnpj, telephone, email, password, address, number, district, cep, city, state, country, convenience, observation, social_reason }, {
+            where: { id: req.params.id }
+        });
+        console.log("foooi");
+        res.redirect('/hotel/');
+    } catch (err) {
+        console.log(err);
+    }
+});
+
 module.exports = router;

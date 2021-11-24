1609bf9f42929f17c23b5ac6826d609a33c24aba
brubandrade <61695979+brubandrade@users.noreply.github.com>
2021-11-24 20:14:39 -0300

    Add files via upload

--- models/Accommodation.js
+++ models/Accommodation.js
@@ -1,3 +1,5 @@
+/*
+Antigo
 const Accommodation1 = (sequelize, DataTypes) => {
     return sequelize.define('Accommodation1', {
         number: DataTypes.STRING,
@@ -9,5 +11,22 @@ const Accommodation1 = (sequelize, DataTypes) => {
         hotel_id: DataTypes.INTEGER,
     });
 }
-
 module.exports = Accommodation1;
+
+*/
+
+module.exports = (sequelize, DataTypes) => {
+    const Accommodation1 = sequelize.define('Accommodation1', {
+        number: DataTypes.STRING,
+        value: DataTypes.FLOAT,
+        number_of_guests: DataTypes.INTEGER,
+        description: DataTypes.STRING,
+        convenience: DataTypes.STRING,
+        observation: DataTypes.STRING,
+        hotel_id: DataTypes.INTEGER
+    }, {});
+    Accommodation1.associate = function(models) {
+        Accommodation1.belongsTo(models.Hotel, { foreignKey: 'hotel_id' })
+    }
+    return Accommodation1;
+}
--- models/Reserves.js
+++ models/Reserves.js
@@ -1,3 +1,5 @@
+/*
+RESERVA:
 const Reserve = (sequelize, DataTypes) => {
     return sequelize.define('Reserve', {
         number: DataTypes.INTEGER,
@@ -11,5 +13,25 @@ const Reserve = (sequelize, DataTypes) => {
         accommodation_id: DataTypes.INTEGER
     });
 }
-
 module.exports = Reserve;
+*/
+
+module.exports = (sequelize, DataTypes) => {
+    const Reserve = sequelize.define('Reserve', {
+        number: DataTypes.INTEGER,
+        initial_date: DataTypes.DATE,
+        final_date: DataTypes.DATE,
+        number_of_guests: DataTypes.INTEGER,
+        observation: DataTypes.STRING,
+        status: DataTypes.STRING,
+        hotel_id: DataTypes.INTEGER,
+        user_id: DataTypes.INTEGER,
+        accommodation_id: DataTypes.INTEGER
+    }, {});
+    Reserve.associate = function(models) {
+        Reserve.belongsTo(models.Hotel, { foreignKey: 'hotel_id' }),
+            Reserve.belongsTo(models.User, { foreignKey: 'user_id' }),
+            Reserve.belongsTo(models.Accommodation1, { foreignKey: 'accommodation_id' })
+    }
+    return Reserve;
+}

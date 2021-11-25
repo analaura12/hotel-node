420822170093c9552101d40ea126d818df8745ab
brubandrade <61695979+brubandrade@users.noreply.github.com>
2021-11-25 20:26:09 -0300

    Add files via upload

--- controllers/index.js
+++ controllers/index.js
@@ -3,13 +3,15 @@ const usersController = require('./usersController');
 const accommodationController = require('./accommodationController');
 const reservesController = require('./reservesController');
 const registrationsController = require('./registrationsController');
+const loginsController = require('./loginsController');
 
 controllers = {
     hotels: hotelsController,
     users: usersController,
     accommodation: accommodationController,
     reserves: reservesController,
-    registrations: registrationsController
+    registrations: registrationsController,
+    logins: loginsController,
 }
 
 module.exports = controllers;

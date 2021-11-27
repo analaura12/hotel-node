973fa627bb696bbad7501b901255ead70a64058a
brubandrade <61695979+brubandrade@users.noreply.github.com>
2021-11-27 14:48:22 -0300

    Add files via upload

--- seeders/2-user.js
+++ seeders/2-user.js
@@ -5,16 +5,28 @@ module.exports = {
         /** Add seed commands here.*/
 
         await queryInterface.bulkInsert('users', [{
-            first_name: 'User',
-            last_name: 'Admin',
-            cpf: '00000000000',
-            birth_date: '2000-10-10',
-            cellphone: '12900000000',
-            city: 'sem cidade',
-            state: 'sem estado',
-            password: 'admin123',
-            email: 'admin@admin.com'
-        }], {});
+                first_name: 'User',
+                last_name: 'Admin',
+                cpf: '00000000000',
+                birth_date: '2000-10-10',
+                cellphone: '12900000000',
+                city: 'sem cidade',
+                state: 'SP',
+                password: 'admin123',
+                email: 'admin@admin'
+            },
+            {
+                first_name: 'Bruna',
+                last_name: 'Lima',
+                cpf: '43350958800',
+                birth_date: '2001-10-03',
+                cellphone: '1299538379',
+                city: 'São José dos Campos',
+                state: 'SP',
+                password: 'admin123',
+                email: 'teste@teste'
+            },
+        ], {});
 
     },
 
--- seeders/4-reservation.js
+++ seeders/4-reservation.js
@@ -5,15 +5,36 @@ module.exports = {
         /** Add seed commands here.*/
 
         await queryInterface.bulkInsert('reserves', [{
-            number: '20',
-            initial_date: '2021-12-10',
-            final_date: '2021-12-20',
-            number_of_guests: 2,
-            status: 'Aprovada',
-            hotel_id: 1,
-            user_id: 1,
-            accommodation_id: 1
-        }], {});
+                number: '10',
+                initial_date: '2021-12-10',
+                final_date: '2021-12-20',
+                number_of_guests: 2,
+                status: 'Aprovada',
+                hotel_id: 2,
+                user_id: 1,
+                accommodation_id: 1
+            },
+            {
+                number: '20',
+                initial_date: '2021-12-22',
+                final_date: '2021-12-24',
+                number_of_guests: 2,
+                status: 'Aprovada',
+                hotel_id: 2,
+                user_id: 2,
+                accommodation_id: 1
+            },
+            {
+                number: '30',
+                initial_date: '2021-12-24',
+                final_date: '2021-12-30',
+                number_of_guests: 2,
+                status: 'Pendente',
+                hotel_id: 3,
+                user_id: 2,
+                accommodation_id: 2
+            },
+        ], {});
 
     },
 

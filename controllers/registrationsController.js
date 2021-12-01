b1ee69a5b2b7c295cb9aebdea6c3276b8e76ffdc
brubandrade <61695979+brubandrade@users.noreply.github.com>
2021-12-01 20:19:54 -0300

    Add files via upload

--- controllers/registrationsController.js
+++ controllers/registrationsController.js
@@ -3,6 +3,9 @@ const { Router } = require('express');
 const multer = require('./multer');
 const router = Router();
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 router.get('/hotel', (req, res) => {
     res.render('hotel/registration');
 });
@@ -10,6 +13,7 @@ router.post('/hotel', multer.single('photo'), async(req, res) => {
     const { company_name, cnpj, telephone, email, password, address, district, cep, number, city, state, country } = req.body;
     const photo = req.file.filename;
     try {
+        password = await bcrypt.hash(password, saltRounds);
         await Hotel.create({ company_name, cnpj, telephone, email, password, address, district, cep, number, city, state, country, photo });
         res.redirect('/login');
     } catch (err) {
@@ -23,6 +27,7 @@ router.get('/user', (req, res) => {
 router.post('/user', async(req, res) => {
     const { first_name, last_name, cpf, birth_date, cellphone, city, state, password, email } = req.body;
     try {
+        password = await bcrypt.hash(password, saltRounds);
         await User.create({ first_name, last_name, cpf, birth_date, cellphone, city, state, password, email });
         res.redirect('/login');
     } catch (err) {
